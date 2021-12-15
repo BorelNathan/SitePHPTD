7be61d6a83a328ad3303a6751e1b340f0b7d9487
BorelNathan <nathan.champommier-borel@etu.univ-amu.fr>
2021-12-15 21:14:07 +0100

    premier question TD3 fait

--- index.php
+++ index.php
@@ -6,6 +6,12 @@ $ipserv = $_SERVER['HTTP_HOST'];
 $servsoft = $_SERVER['SERVER_SOFTWARE'];
 $dateEN = date('m-d-Y');
 $dateFR = date('d-m-Y');
+$connection = 'Se connecter';
+$pageconnection = 'login.php';
+$calculator = 'La calculatrice si tu clique là :3 ';
+$pagecalculator = 'calculator.php' ;
+$BingChilling = 'Bing Chilling ';
+$BingChillingLien = 'https://www.youtube.com/watch?v=AWOyEIuVzzQ';
 ?>
 
 
@@ -20,7 +26,6 @@ $dateFR = date('d-m-Y');
 
     <?php
 
-
       echo '<p class="titre">Nyaaaa</p><br/>';
       echo '<p class="titre">D\'ailleurs ton ip c\'est ' . "$iplocal" . ' gros bakaaaa >///< ' . '</br>';
       echo 'D\'ailleurs l\'ip du serveur c\'est ' . "$ipserv" . '</br>';
@@ -28,11 +33,14 @@ $dateFR = date('d-m-Y');
       echo '<p class="titre" >' . "\n";
       echo $dateEN . ' <- la date en anglais ' . '<br/>' . "\n";
       echo $dateFR . ' <- la date en français cette fois ' . '<br/>' . "\n";
-      echo '<a class="hyperliens" class="titre" target="_blank" href="https://www.youtube.com/watch?v=AWOyEIuVzzQ">Bing Chilling </a>' . '<br/>';
-      echo 'Une surpise si tu clique (calculator)<a class="hyperliens" target="_blank" href="calculator.php"> là :3 </a>' . '<br/>';
+
+    ?>
+
+      <a class="hyperliens" class="titre" target="_blank" href="<?php echo $BingChillingLien; ?>"> <?php echo $BingChilling; ?> </a> <br/>
+      <a class="hyperliens" target="_blank" href="<?php echo $pagecalculator; ?>"> <?php echo $calculator; ?> </a> <br/>
 
 
-     ?>
+
      <br/>
 </div>
 
@@ -43,13 +51,13 @@ $dateFR = date('d-m-Y');
 
 
      <form action="data_processing.php" method="POST" name="formulaire">
-      <a class="inputtext"> <input required="required" type="text" name="Identifiant" /> Identifiant <br/>
-      <a class="sexe"> <input type="radio" name="sexe" value="M"/>M
-      <a class="sexe"> <input type="radio" name="sexe" value="F"/>F <br/>
-      <a class="inputtext"> <input required="required" type="text" name="E-mail" /> E-mail  <br/>
-      <a class="inputtext"> <input required="required" type="password" name="mot de passe1" /> Mot de passe <br/>
-      <a class="inputtext"> <input required="required" type="password" name="mot de passe2" /> Vérifier le mot de passe <br/>
-      <a class="inputtext"> <input required="required" type="text" name="telephone" /> Numéro de téléphone <br/>
+      <p class="inputtext"> <input required="required" type="text" name="Identifiant" /> Identifiant <br/>
+      <p class="sexe"> <input type="radio" name="sexe" value="M"/>Homme
+      <p class="sexe"> <input type="radio" name="sexe" value="F"/>Femme <br/>
+      <p class="inputtext"> <input required="required" type="text" name="E-mail" /> E-mail  <br/>
+      <p class="inputtext"> <input required="required" type="password" name="mot de passe1" /> Mot de passe <br/>
+      <p class="inputtext"> <input required="required" type="password" name="mot de passe2" /> Vérifier le mot de passe <br/>
+      <p class="inputtext"> <input required="required" type="text" name="telephone" /> Numéro de téléphone <br/>
       <br/>
       <label for="pays">Choisir un pays : </label> <br/>
       <select name="pays" id="choix-pays">
@@ -65,7 +73,7 @@ $dateFR = date('d-m-Y');
      <input type="checkbox" name="CG" required="required"/>  <br/>
      <input class="button" type="submit" name="action" value="S'inscrire" /> <br/>
    </form><br/>
-     <a class="connection" href="login.php">Se connecter </a>
+     <a class="connection" href="<?php echo $pageconnection; ?>"> <?php echo $connection; ?> </a>
    </p>
 </div>
 
